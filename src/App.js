6e78f0a38c6b54e37a08e1c07e27683a7bd4107c
HarshDeep61034 <hdeep61034@gmail.com>
2023-04-01 00:39:33 +0530

    initialized React and tailwind in Project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-4xl text-red-800">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
